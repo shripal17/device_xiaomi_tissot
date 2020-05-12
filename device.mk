20d2bbe38a82c5673dfae844cfc159acfb998e3f
Shripal Jain <shripal17@gmail.com>
2020-05-12 20:30:23 +0530

    tissot: Remove Via browser
    Signed-off-by: Shripal Jain <shripal17@gmail.com>

--- device.mk
+++ device.mk
@@ -109,8 +109,3 @@ PRODUCT_PACKAGES_DEBUG += \
 # Verity
 PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/soc/7824900.sdhci/by-name/system
 $(call inherit-product, build/target/product/verity.mk)
-
-# Via
-PRODUCT_PACKAGES += \
-    Via
-
